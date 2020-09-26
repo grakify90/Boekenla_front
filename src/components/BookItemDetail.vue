2cc3eb2151c54a9fbad2c461686e36a99a47ad4a
grakify90 <ninavanes1990@hotmail.com>
2020-09-26 12:57:33 +0200

    changed styling of detail book page

--- src/components/BookItemDetail.vue
+++ src/components/BookItemDetail.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <h1>{{ book.title }}</h1>
-    <h1>{{ book.author }}</h1>
-    <h1>{{ book.description }}</h1>
-    <h1>{{ book.genre }}</h1>
-    <h1>{{ book.price }} euro</h1>
-    <h1>Bidding allowed: {{ book.allowBid }}</h1>
-    <img v-bind:src="book.imageUrl" />
+  <div
+    class="p-10 grid grid-cols-2 gap-4 container mx-auto sm:w-11/12 md:w-1/2 lg:w-1/4"
+  >
+    <div class="bg-black w-4/5 flex align-items-center">
+      <img v-bind:src="book.imageUrl" />
+    </div>
+    <div>
+      <h1>{{ book.title }}</h1>
+      <h1>{{ book.author }}</h1>
+      <h1>{{ book.description }}</h1>
+      <h1>{{ book.genre }}</h1>
+      <h1>{{ book.price }} euro</h1>
+      <h1>Bidding allowed: {{ book.allowBid }}</h1>
+    </div>
   </div>
 </template>
 
